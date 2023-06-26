bdbaf467b229a43500e4a064c8e95e11454e1072
keeduardo607 <keeduardo607@gmail.com>
2023-06-26 17:06:17 -0300

    (funcionalidade) Os dados do carrinho podem ser salvos no localStorage

--- projeto-labecommerce/src/componentes/ShoppingCart/Cart/carrinho.jsx
+++ projeto-labecommerce/src/componentes/ShoppingCart/Cart/carrinho.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Itens from "../Items/itens";
 import { ContainerCarrinho } from "./carrinhoStyle";
 
 export default function Carrinho({ cart, setCart }) {
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart, setCart]);
+
+  useEffect(() => {
+    const savedCart = localStorage.getItem("cart");
+    if (savedCart) {
+      setCart(JSON.parse(savedCart));
+    }
+  }, [setCart]);
 
   return (
     <ContainerCarrinho>
@@ -10,6 +20,3 @@ export default function Carrinho({ cart, setCart }) {
     </ContainerCarrinho>
   );
 }
-
-
-
