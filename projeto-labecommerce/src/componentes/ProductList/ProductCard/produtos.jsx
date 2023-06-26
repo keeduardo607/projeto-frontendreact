d09c20cc79ed0901beb3f4763fbf3adb82501980
keeduardo607 <keeduardo607@gmail.com>
2023-06-26 16:30:46 -0300

    alterações básicas

--- projeto-labecommerce/src/componentes/ProductList/ProductCard/produtos.jsx
+++ projeto-labecommerce/src/componentes/ProductList/ProductCard/produtos.jsx
@@ -1,42 +1,46 @@
 import React from "react";
 import { ContainerProdutos } from "./produtosStyle";
 
-   
-  export default function Produtos ({products, searchFilter, minFilter, setMinFilter, maxFilter, setMaxFilter, ordination, setOrdination}) {
+  export default function Produtos ({products, searchFilter, minFilter, maxFilter, ordination, cart, setCart, handleAdicionarAoCarrinho}) {
+
 
   return (
     <ContainerProdutos>
-    {products
-      .filter((item) => { 
-        if(item.value >= minFilter && item.value <= maxFilter){
-        return item
-        } else if (!minFilter && !maxFilter){
-        return products
-        }
-      })
-      .sort(() => {
-        if (ordination === 'asc') {
-        return 0
-        } else {
-        return -1
-        }
-      })
-      .filter((item) => {
-        if(item.name.toUpperCase().includes(searchFilter.toUpperCase())){
-        return item
-        } else if (!searchFilter){
-        return products
-        }
-      })
-      .map((item) => (
-        <div key={item.id}>
-         <img src={item.imageUrl} alt={item.name} />
-          <p>{item.id}</p>
-          <p id="name"> {item.name}</p>
-          <p id="value">  R$ {item.value}</p>
-         <button>Adicionar ao Carrinho</button>
-       </div>
-     ))}
-  </ContainerProdutos>
+      {products
+        .filter((item) => { 
+          if((minFilter && maxFilter) && (item.value >= minFilter && item.value <= maxFilter) ){
+            return item
+          } else if (minFilter && !maxFilter && item.value >= minFilter){
+            return item
+          } else if (!minFilter && maxFilter && item.value <= maxFilter) {
+            return item
+          } else if (!minFilter && !maxFilter){
+            return products
+          } else {
+            return false
+          }})
+        .sort(() => {
+          if (ordination === 'asc') {
+          return 0
+          } else {
+          return -1
+          } 
+         })
+        .filter((item) => {
+          if(item.name.toUpperCase().includes(searchFilter.toUpperCase())){
+          return item
+          } else if (!searchFilter){
+          return products
+          } return false
+         })
+        .map((item, index) => (
+         <div key={index}>
+          <img src={item.imageUrl} alt={item.name} />
+           <p id="name"> {item.name}</p>
+           <p id="value">  R$ {item.value}</p>
+          <button onClick={() => handleAdicionarAoCarrinho(item)}>Adicionar ao Carrinho</button>
+         </div>
+        ))}
+   </ContainerProdutos>
   );
 }
--- projeto-labecommerce/src/componentes/ShoppingCart/Cart/carrinho.jsx
+++ projeto-labecommerce/src/componentes/ShoppingCart/Cart/carrinho.jsx
@@ -1,19 +1,62 @@
 import React from "react";
 import Itens from "../Items/itens";
-import { ContainerCarrinho, H2Carrinho } from "./carrinhoStyle";
+import { ContainerCarrinho } from "./carrinhoStyle";
 
+export default function Carrinho({ cart, setCart }) {
 
-export default function Carrinho () {
-    
   return (
-  <ContainerCarrinho>
-    <H2Carrinho>
-      <p>Carrinho:</p>
-    </H2Carrinho>
-   
-   <Itens />
-
-  </ContainerCarrinho>
+    <ContainerCarrinho>
+      <Itens cart={cart} setCart={setCart} />
+    </ContainerCarrinho>
   );
 }
 
+
+import {
+  ContainerItens,
+  ItemContainer,
+  ItemInfo,
+  ItemName,
+  ItemActions,
+  RemoveButton,
+  TotalContainer
+} from "../Items/ItemsStyled";
+
+
+export default function Itens({ cart, setCart }) {
+  const handleRemoverDoCarrinho = (produto) => {
+    const novoCarrinho = cart.filter((item) => item.id !== produto.id);
+    setCart(novoCarrinho);
+  };
+
+  const calcularValorTotal = () => {
+    const valorTotal = cart.reduce((total, item) => {
+      return total + item.value * item.quantidade;
+    }, 0);
+
+    return valorTotal.toFixed(2);
+  };
+
+  return (
+    <ContainerItens>
+      <h3>Carrinho: </h3>
+      {cart.map((item) => (
+        <ItemContainer key={item.id}>
+          <ItemInfo>
+            <ItemName>{item.name}</ItemName>
+            <p>Quantidade: {item.quantidade}</p>
+            <p>Preço: R$ {item.value.toFixed(2)}</p>
+          </ItemInfo>
+          <ItemActions>
+            <RemoveButton onClick={() => handleRemoverDoCarrinho(item)}>
+              Remover
+            </RemoveButton>
+          </ItemActions>
+        </ItemContainer>
+      ))}
+      <TotalContainer>
+        <p>Valor Total: R${calcularValorTotal()}</p>
+      </TotalContainer>
+    </ContainerItens>
+  );
+}
